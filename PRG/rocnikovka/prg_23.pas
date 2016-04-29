0c82407ce73d8fd77d79098443cf61af40f43502
Logotrop <keramdobes@gmai.com>
2016-04-29 10:30:50 +0200

    prg - rocnikovka
    jen ja

--- PRG/rocnikovka/prg_23.pas
+++ PRG/rocnikovka/prg_23.pas
@@ -1,8 +1,8 @@
 program prg_23;
 uses Crt;
 
-const n=5;
-var poleA,poleB :array [1..n,1..n] of real;
+const n=2;
+var poleA,poleB,poleC :array [1..n,1..n] of real;
   i,j : integer;
 
 
@@ -64,7 +64,55 @@ procedure pokracovat;
 begin
   writeln('pokracujte stisknutim libovolne klavesy...');
   readkey;
+  ClrScr;
   end;
+
+procedure nasobeni;
+var k:integer;
+begin
+ begin
+for i:=1 to n do
+    for j:=1 to n do
+    begin
+      poleC[i,j]:=0;
+      for k:=1 to n do poleC[i,j]:=PoleC[i,j]+poleA[i,k]*poleB[k,j];
+    end;
+ end;
+ writeln;
+  writeln ('soucin MaticeA * MaticeB =');
+  writeln;
+  for i:= 1 to n do
+  for j:=1 to n do
+  begin
+    write(poleC[i,j]:6:0);
+    if (j=n) then writeln
+  end;
+end;
+procedure cisla;
+var maxA,maxB,minA,minB: real;
+  a,b,c,d,e,f,g,h:integer;
+Begin
+  minA:=poleA[1,1];
+  minB:=poleB[1,1];
+  maxA:=poleA[1,1];
+  maxB:=poleB[1,1];
+  for i:= 1 to n do
+  for j:=1 to n do
+  begin
+    if (poleA[i,j]<=minA)then begin minA:=poleA[i,j]; a:=i; b:=j;end
+    else if (poleA[i,j]>=maxA)then begin maxA:=poleA[i,j]; c:=i; d:=j; end;
+    if (poleB[i,j]<=minB)then begin minB:=poleB[i,j]; e:=i; f:=j; end
+    else if (poleB[i,j]>=maxB)then begin maxB:=poleB[i,j]; g:=i; h:=j; end;
+  end;
+writeln;
+writeln('Matice A:');
+writeln('nejvetsi:',maxA:4:0,' na pozici [',c,',',d,']');
+writeln('nejmensi:',minA:4:0,' na pozici [',a,',',b,']');
+writeln;
+writeln('Matice B:');
+writeln('nejvetsi:',maxB:4:0,' na pozici [',g,',',h,']');
+writeln('nejmensi:',minB:4:0,' na pozici [',e,',',f,']');
+end;
 
 begin
 randomize;
@@ -73,6 +121,9 @@ vypis;
 shodne;
 pokracovat;
 vypis;
+nasobeni;
 pokracovat;
-repeat until keypressed;
+vypis;
+cisla;
+pokracovat;
 end.
